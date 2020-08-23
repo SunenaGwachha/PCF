e8c25f1070a283a17e12fba93ae4578e08c8ffa7
SunenaGwachha <51094691+SunenaGwachha@users.noreply.github.com>
2020-08-23 18:27:14 -0500

    added

--- Pcf_serviceregistry/address/src/main/java/com/example/address/AddressController.java
+++ Pcf_serviceregistry/address/src/main/java/com/example/address/AddressController.java
@@ -1,14 +1,28 @@
 package com.example.address;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AddressController {
 
+    @Autowired
+    private MyFeignClient myFeignClient;
+
     @GetMapping("/address")
     public String getAddress(){
         return "Address From Address Controller";
+    }
+
+
+    @GetMapping("/getNameFromStudentService")
+    public String getStudentName(){
+
+
+        return myFeignClient.getStdName();
+
+
     }
 
 }
--- Pcf_serviceregistry/address/src/main/java/com/example/address/MyFeignClient.java
+++ Pcf_serviceregistry/address/src/main/java/com/example/address/MyFeignClient.java
@@ -3,10 +3,10 @@ package com.example.address;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(value="ADDRESS-SERVICE")
+@FeignClient(value="STUDENT-SERVICE")
 public interface MyFeignClient {
 
-    @GetMapping(value="getAddress")
-    String getAddress();
+    @GetMapping(value="name")
+    String getStdName();
 
 }
